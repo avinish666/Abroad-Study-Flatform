57fa3af3b6c59baaf82d09b14d09d94b97170efc
Avinish Kumar <avi77sub@gmail.com>
2026-09-22 15:51:03 +0530

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-const API_URL = "http://localhost:4000";
+const API_URL = "https://abroad-study-flatformm.onrender.com/api";
 
 export default function SignUp() {
   const navigate = useNavigate();
