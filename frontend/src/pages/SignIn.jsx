d1834e3f10fc74a73740dbe74cbe65d4c049b1c7
Avinish Kumar <avi77sub@gmail.com>
2026-09-22 15:52:43 +0530

    Update SignIn.jsx

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-const API_URL = "http://localhost:4000";
+const API_URL = "https://abroad-study-flatformm.onrender.com";
 
 export default function SignIn() {
   const navigate = useNavigate();
